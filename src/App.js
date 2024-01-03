46e5ce17481693c7673de3aa497e5057cae8b776
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-03 11:36:49 +0000

    Populated weatherapp directory

--- src/App.js
+++ src/App.js
@@ -4,20 +4,44 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <div className="weather-app">
+          <header>
+            <form>
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                required
+                className="search-input"
+              />
+              <input type="submit" value="Search" className="search-button" />
+            </form>
+          </header>
+          <main>
+            <div>
+              <h1 className="current-city">Paris</h1>
+              <p className="current-details">
+                London, England: light cloud <br />
+                Humidity: 40%, Wind: 30mph
+              </p>
+            </div>
+            <div>
+              <div className="current-temperature">
+                <span>☀️ 24°C</span>
+              </div>
+            </div>
+          </main>
+
+          <footer>
+            <p>
+              This project was coded by <a href="https://github.com/annadowding" target="_blank">Anna Dowding</a>and is<a href="https://github.com/annadowding/reactapp">open-sourced</a>.
+              
+            
+          
+            </p>
+          </footer>
+        </div>
+      </body>
     </div>
   );
 }
