de069f62d348b926c696de0d52cf7dfa7e6b1526
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2024-01-03 12:44:05 +0000

    Removed styles and html from App.css and App.js respectively

--- src/App.js
+++ src/App.js
@@ -3,51 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <body>
-        <div className="weather-app">
-          <header>
-            <form>
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                required
-                className="search-input"
-              />
-              <input type="submit" value="Search" className="search-button" />
-            </form>
-          </header>
-          <main>
-            <div>
-              <h1 className="current-city">Paris</h1>
-              <p className="current-details">
-                London, England: light cloud <br />
-                Humidity: 40%, Wind: 30mph
-              </p>
-            </div>
-            <div>
-              <div className="current-temperature">
-                <span>☀️ 24°C</span>
-              </div>
-            </div>
-          </main>
-
-          <footer>
-            <p>
-              This project was coded by{" "}
-              <a href="https://github.com/annadowding">
-                Anna Dowding
-              </a>
-              and is
-              <a href="https://github.com/annadowding/weatherapp">
-                open-sourced
-              </a>
-              .
-            </p>
-          </footer>
-        </div>
-      </body>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/annadowding">Anna Dowding</a> and is{" "}
+        <a href="https://github.com/annadowding/weatherapp"> open-sourced</a>.
+      </footer>
     </div>
   );
 }
+  
+
 
 export default App;
